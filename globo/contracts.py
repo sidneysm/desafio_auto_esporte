f376e01c53a1b604e271b56507049e05e5c74cfc
sidneysm <sidneysoaresm@gmail.com>
2017-07-30 19:22:14 -0300

    Terminados testes simples da saída do Spider

---
+++ globo/contracts.py
@@ -0,0 +1,48 @@
+from scrapy.contracts import Contract
+from scrapy.exceptions import ContractFail
+
+
+class HasItemContract(Contract):
+
+    name = 'has_itens'
+
+    def post_process(self, output):
+        if 'item' not in output[0]['feed'][0]:
+            raise ContractFail('Não nenhum item na saída')
+
+
+class HasTitleItem(Contract):
+
+    name = 'has_title'
+
+    def post_process(self, output):
+        if 'title' not in output[0]['feed'][0]['item']:
+            raise ContractFail('Não há título no primeiro item da saída')
+
+
+class HasLinkItem(Contract):
+
+    name = 'has_link'
+
+    def post_process(self, output):
+        if 'link' not in output[0]['feed'][0]['item']:
+            raise ContractFail('Não há link no primeiro item da saída')
+
+
+class HasDescriptionItem(Contract):
+
+    name = 'has_description'
+
+    def post_process(self, output):
+        if 'description' not in output[0]['feed'][0]['item']:
+            raise ContractFail('Não há descrição no primeiro item da saída')
+
+
+class HasDescriptionSomething(Contract):
+
+    name = 'has_description_somithing'
+
+    def post_process(self, output):
+        desc = output[0]['feed'][0]['item']['description']
+        if len(desc) < 1:
+            raise ContractFail('A descrição ficou vazia')
